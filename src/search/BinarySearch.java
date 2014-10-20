fc8ef743e944551dcdac74522a0de9baa64ad5e5
goki-libra <goki.sriram@gmail.com>
2014-10-19 22:53:39 -0700

    Binary search commit

--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -1,36 +1,63 @@
 package search;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class BinarySearch {
+public class BinarySearch
+{
 
-	public static void main(String[] args) {
-		System.out.println("Hello World- from Gokul's IAir");
-		List<Integer> list = Arrays.asList(0,1,2,3,4,5,6,7,8);
-		binarySearch(list, 7);
-	}
-	
-	public static void binarySearch(List<Integer> integers, int search){
-		int size = integers.size();
-		int min = 0; int max = size-1;
-		while (true){
-			if (integers.get(min + size/2) == search){
-				System.out.println("Item found at location: " + size/2);
-				break;
-			}
-			else if (integers.get(min + size/2) > search){
-				System.out.println("Item found  is :" + integers.get(size/2));
-				max = size/2;
-				size = max - min;
-			}
-			else {
-				System.out.println("Item found is :" + integers.get(size/2));
-				min = size/2;
-				size = max - min;
-			}
-		}
-	}
-	
+   public static void main(String[] args)
+   {
+      List<Integer> list = Arrays.asList(0, 1, 2, 3, 4);
+      binarySearchIterative(list, 3);
+      if (!binarySearchRecursive(list, 0, list.size() - 1, 4))
+         System.out.println("Item not found in the list in recursive approach");
+   }
+
+   private static boolean binarySearchRecursive(List<Integer> list,
+                                                int min,
+                                                int max,
+                                                int search)
+   {
+      int mid = (min + max) / 2;
+      boolean result = false;
+      if (min > max) {
+         return false;
+      } else if (list.get(mid) == search) {
+         System.out.println("Item found at index: " + mid + " in recursive approach");
+         return true;
+      } else if (list.get(mid) < search) {
+//         System.out.println("Item NOT found at index: " + mid);
+         result = result || binarySearchRecursive(list, mid + 1, max, search);
+      } else {
+//         System.out.println("Item NOT found at index: " + mid);
+         max = mid - 1;
+         result = result || binarySearchRecursive(list, min, mid - 1, search);
+      }
+      return result;
+   }
+
+   private static void binarySearchIterative(List<Integer> list,
+                                             int search)
+   {
+      int mid = 0, min = 0, max = list.size() - 1;
+      while (min <= max) {
+         mid = (min + max) / 2;
+         if (list.get(mid) == search) {
+            System.out.println("Item found at index: " + mid + " in iterative approach");
+            return;
+         } else if (list.get(mid) < search) {
+//            System.out.println("Item NOT found at index: " + mid);
+            min = mid + 1;
+
+         } else {
+//            System.out.println("Item NOT found at index: " + mid);
+            max = mid - 1;
+         }
+
+      }
+      System.out.println("Item NOT found in list in iterative approach");
+
+   }
+
 }
